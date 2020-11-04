b1248da7ea470de9f4ad0977b0b77ac38c7761af
yeomyeom <whodoctor9938@gmail.com>
2020-11-04 14:53:16 +0900

    cookie is used to store endtime

--- payload.js
+++ payload.js
@@ -1,3 +1,2 @@
 var currentUrl = location.href; // current url
-currentUrl = JSON.parse(JSON.stringify(currentUrl))
-chrome.runtime.sendMessage(currentUrl)
+chrome.runtime.sendMessage({'url': currentUrl})
--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var timerIsAlive
 var serverUrl
-
+/*
 window.addEventListener('load', function (evt) {
 	chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
 		file: 'payload.js'
@@ -9,11 +9,31 @@ window.addEventListener('load', function (evt) {
 chrome.runtime.onMessage.addListener(function(message){
     serverUrl = message
 })
-
+*/
 window.onload = function(){
-    makeList()
-    document.getElementById('start').addEventListener('click', ActiveButton)
-    document.getElementById('stop').addEventListener('click', DeactivButton)
+    chrome.extension.getBackgroundPage().chrome.tabs.executeScript(null, {
+		file: 'payload.js'
+    })
+    chrome.runtime.onMessage.addListener(function(message){
+        serverUrl = message['url']
+        try{
+            chrome.cookies.get({'name': 'time', 'url': String(serverUrl)}, function (cookie){
+                //alert('yes cookie')
+                if(cookie){
+                    val = JSON.parse(cookie.value)
+                    stopTime = new Date(val['stop'])
+                    getservertime(serverUrl, stopTime)
+                }else{
+                    killCookie()
+                    //alert('no cookie')
+                    makeList()
+                }
+            })
+        }catch (error){
+            //alert('no cookie' + String(error))
+            makeList()
+        }
+    })    
 }
 
 function makeList(){
@@ -26,14 +46,22 @@ function makeList(){
         $('#minOption').append('<option value="'+i+'">'+String(i)+'</option>')
         $('#secOption').append('<option value="'+i+'">'+String(i)+'</option>')
     }
+    document.getElementById('start').addEventListener('click', ActiveButton)
+    document.getElementById('stop').addEventListener('click', DeactivButton)
 }
 function ActiveButton(){
     var stopTime = selectBoxVal()
     document.getElementById('start').style.display='none'
+    document.getElementById('message').innerHtml = ' '
     getservertime(serverUrl, stopTime)//location.href==chrome extension
 }
 function DeactivButton(){
     timerIsAlive = false
+    document.getElementById('message').innerHTML = "STOP"
+    document.getElementById('start').style.display='none'
+    document.getElementById('stop').style.display='none'
+    //alert('delete cookie')
+    killCookie()
 }
 
 function getservertime(serverurl, stopTime){
@@ -43,13 +71,16 @@ function getservertime(serverurl, stopTime){
         success: function(req, Status, xhr){
             var serverTime = xhr.getResponseHeader('Date')
             var localTime = Date.parse(serverTime)
-            localTime = new Date(localTime)
-            var koreaTime = localTime.toLocaleString('en-US', {timeZone: 'Asia/Seoul'})
+            startTime = new Date(localTime)
+            var koreaTime = startTime.toLocaleString('en-US', {timeZone: 'Asia/Seoul'})
             //var koreaTime = localTime.toLocaleString('en-US', {timeZone: 'America/New_York'})
             //Servertime : Tue, 03 Nov 2020 07:06:45 GMT
             //localTime : Tue Nov 03 2020 16:06:45 GMT+0900 (대한민국 표준시)
             //Server Time : 11/3/2020, 4:06:45 PM
-            startTime = localTime
+            //alert('set cookie')
+            chrome.cookies.set({'url': String(serverUrl), 'name': 'time',
+                'value': JSON.stringify({'stop': stopTime}), 
+                'expirationDate': stopTime.getTime()})
             timerStart(startTime, stopTime)
         },
         error: function(e){
@@ -66,12 +97,12 @@ function timerStart(startTime, stopTime){
         stopTime.setDate(stopTime.getDate() + 1)
         offset = stopTime - startTime
     }
-    
     timer = setInterval(function(){
         offset = stopTime - startTime
         if(offset <= 0){
             chrome.tabs.reload()//time is over refrash
             document.getElementById("message").innerHTML = "refrash"
+            DeactivButton()
             clearInterval(timer)
         }
         if(!timerIsAlive){
@@ -80,8 +111,9 @@ function timerStart(startTime, stopTime){
         document.getElementById("H").innerText = Math.floor((offset % (day))/(hour))
         document.getElementById("M").innerText = Math.floor((offset % (hour))/(min))
         document.getElementById("S").innerText = Math.floor((offset % (min))/(sec))
-        startTime.setMilliseconds(startTime.getMilliseconds() + 500)
-    }, 500)
+        startTime.setMilliseconds(startTime.getMilliseconds() + 250)
+    }, 250)
+    
 }
 
 function selectBoxVal(){
@@ -93,4 +125,9 @@ function selectBoxVal(){
     let min = $("#minOption option:selected").val()
     let sec = $("#secOption option:selected").val() - 1 //for system delay
     return new Date(year, month, day, hour, min, sec, 0)
+}
+
+function killCookie(){
+    chrome.cookies.remove({'name': 'time', 'url': String(serverUrl)})
+    chrome.cookies.remove({'name': 'time', 'url': String(serverUrl)})
 }
